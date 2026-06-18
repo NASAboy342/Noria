dd7b8a76e537738e6d619f5229a1af12d5a257a9
NASAboy342 <sopheakpin342@gmail.com>
2026-06-18 21:49:12 +0700

    fix

--- NoriaFE/NoriaFE/src/router/index.ts
+++ NoriaFE/NoriaFE/src/router/index.ts
@@ -1,13 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+import Dashboard from '../pages/dasboard.vue'
+import DashboardMobile from '../pages/dashboardMobile.vue'
+
+import Rooms from '../pages/rooms.vue'
+import RoomsMobile from '../pages/roomsMobile.vue'
+
+import RoomDetail from '../pages/roomDetail.vue'
+import RoomDetailMobile from '../pages/roomDetailMobile.vue'
+
+import RoomPaymentDetail from '../pages/roomPaymentDetail.vue'
+
 const isMobile = /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(
   navigator.userAgent
 )
 
 const routes = [
-  { path: '/', name: 'ជ្រើសរើសអគារ', component: () => import(isMobile ? '../pages/dashboardMobile.vue' : '../pages/dasboard.vue') },
-  { path: '/rooms', name: 'បន្ទប់ជួល', component: () => import(isMobile ? '../pages/roomsMobile.vue' : '../pages/rooms.vue') },
-  { path: '/roomDetail', name: 'ព័ត៌មានបន្ទប់', component: () => import(isMobile ? '../pages/roomDetailMobile.vue' : '../pages/roomDetail.vue') },
-  { path: '/roomPaymentDetail', name: 'វិក្កយបត្រ', component: () => import('../pages/roomPaymentDetail.vue') },
+  {
+    path: '/',
+    name: 'ជ្រើសរើសអគារ',
+    component: isMobile ? DashboardMobile : Dashboard
+  },
+  {
+    path: '/rooms',
+    name: 'បន្ទប់ជួល',
+    component: isMobile ? RoomsMobile : Rooms
+  },
+  {
+    path: '/roomDetail',
+    name: 'ព័ត៌មានបន្ទប់',
+    component: isMobile ? RoomDetailMobile : RoomDetail
+  },
+  {
+    path: '/roomPaymentDetail',
+    name: 'វិក្កយបត្រ',
+    component: RoomPaymentDetail
+  }
 ]
 
 export const router = createRouter({
